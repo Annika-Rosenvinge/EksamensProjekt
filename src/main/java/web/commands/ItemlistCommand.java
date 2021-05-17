3702dbba28da631c2e5e04c8d87735642f5e2b5e
Florian-Schaffer <florian.schaffer@hotmail.com>
2021-05-17 12:59:43 +0200

    itemlist done

--- src/main/java/web/commands/ItemlistCommand.java
+++ src/main/java/web/commands/ItemlistCommand.java
@@ -14,6 +14,7 @@ public class ItemlistCommand extends CommandProtectedPage {
     Database database;
 
 
+
     public ItemlistCommand(String pageToShow, String role) {
         super(pageToShow, role);
         this.itemlistFacade= new ItemlistFacade(database);
@@ -23,14 +24,34 @@ public class ItemlistCommand extends CommandProtectedPage {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException{
         request.getContextPath();
         Material material = new Material(request.getContentType(), request.getContentLength(), request.getContentType(), request.getContentLength());
-        int material_id = material.getId();
+        int order_id;
+        int material_id;
+        //ItemList itemList;
+        int length;
+        int quantity;
+        String description;
+        double price;
 
-        ItemList itemList;
+        try{
+            order_id = Integer.parseInt(request.getParameter(""));
+            material_id = Integer.parseInt(request.getParameter(""));
+            length = Integer.parseInt(request.getParameter("Skriv længden på carporten"));
+            quantity = Integer.parseInt(request.getParameter("Skriv antallet af brædder"));
+            description = request.getParameter("");
+            price = Double.parseDouble(request.getParameter("Indtast prisen"));
+
+        } catch (NumberFormatException e) {
+            request.setAttribute("ERROR","Du skal indtaste heltal");
+            return "insertpage";
+        }
+        request.setAttribute("length", length);
+        request.setAttribute("quantity", quantity);
+        request.setAttribute("description", description);
+        request.setAttribute("price", price);
+        request.setAttribute("material_id",material_id);
 
 
-
-
-
+        itemlistFacade.createItemList(request.getContentLength(), material_id,length,quantity, description, price, order_id);
         return pageToShow;
     }
 
